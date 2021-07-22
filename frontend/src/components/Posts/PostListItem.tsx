39dea103368467762cb66100eb6405e898fda13b
Camden Mecklem <c@camdenmecklem.com>
2021-07-21 22:35:55 -0400

    remove unneeded styles from post list item to fix overscroll on mobile

--- frontend/src/components/Posts/PostListItem.tsx
+++ frontend/src/components/Posts/PostListItem.tsx
@@ -15,22 +15,16 @@ interface Props {
 
 const PostCard: React.FC<Props> = ({ groupID, post }) => (
   <Link className='hoverable-item' key={post.id} to={`/groups/${groupID}/${post.id}`}>
-    <div className='card mt-4 mb-4'>
+    <div className='card my-4'>
       <div className='card-content'>
         <div className='content'>
-          <article className='media'>
-            <div className='media-content'>
-              <div className='content'>
-                <p>
-                  <strong className='has-text-primary is-size-5'>{post.title}</strong>
-                  <br />
-                  <strong>{truncate(post.text)}</strong>
-                  <br />
-                  posted {dayjs().to(dayjs(post.createdAt))} by <strong>{post.User.displayName}</strong> &#183; {post.replies ? post.replies.length : 0} replies
-                </p>
-              </div>
-            </div>
-          </article>
+          <p>
+            <strong className='has-text-primary is-size-5'>{post.title}</strong>
+            <br />
+            <strong>{truncate(post.text)}</strong>
+            <br />
+            posted {dayjs().to(dayjs(post.createdAt))} by <strong>{post.User.displayName}</strong> &#183; {post.replies ? post.replies.length : 0} replies
+          </p>
         </div>
       </div>
     </div>
