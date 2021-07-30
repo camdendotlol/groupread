8457385029fe59d22d15d84cb73b0472309fe6f3
Camden Mecklem <c@camdenmecklem.com>
2021-07-30 18:46:06 -0400

    move global styles to styled component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,17 @@ const PostView = React.lazy(() => import('./components/Posts'))
 const GroupScheduler = React.lazy(() => import('./components/GroupScheduler'))
 const Compatibility = React.lazy(() => import('./components/Compatibility'))
 import './App.scss'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: $family-sans-serif, sans-serif;
+  }
+
+  ::selection {
+    background: rgba(137, 207, 240, 0.5);
+  }
+`
 
 const App: React.FC = () => {
   const dispatch = useAppDispatch()
@@ -25,6 +36,7 @@ const App: React.FC = () => {
 
   return (
     <>
+      <GlobalStyle />
       <Navbar />
       {/* mobile-container navbar-offset doesn't do anything unless the user on on a small screen,
       in which case it provides side margins to all content, Unfortunately, it messes up
